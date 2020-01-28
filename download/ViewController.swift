3a50713304b1422a9af710c87b60793ef178451b
sabrinasturtevant <sabrina.sturteva@smail.astate.edu>
2020-01-28 12:48:02 -0600

    first commit. want to add web view

--- download/ViewController.swift
+++ download/ViewController.swift
@@ -14,7 +14,49 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func firstButtonTapped(_ sender: Any) {
+        performSegue(withIdentifier: "first", sender: nil)
+    }
+    
+    
+    @IBAction func secondButtonTapped(_ sender: Any) {
+        performSegue(withIdentifier: "second", sender: nil)
+    }
+    
+    @IBAction func thirdButtonTapped(_ sender: Any) {
+        performSegue(withIdentifier: "third", sender: nil)
+    }
+    
+    @IBAction func forthButtonTapped(_ sender: Any) {
+        performSegue(withIdentifier: "fourth", sender: nil)
+    }
+    
+    @IBAction func backButton(_ sender: Any) {
+        performSegue(withIdentifier: "back", sender: nil)
+    }
+    
+    @IBAction func fifthButtonTapped(_ sender: Any) {
+        performSegue(withIdentifier: "fifth", sender: nil)
+    }
+    
+    @IBAction func sixthButtonTapped(_ sender: Any) {
+        performSegue(withIdentifier: "sixth", sender: nil)
+    }
+    @IBAction func backAgain(_ sender: Any) {
+        performSegue(withIdentifier: "backTwo", sender: nil)
+    }
+    
+    
+    @IBAction func overloadButton(_ sender: Any) {
+        performSegue(withIdentifier: "seventh", sender: nil)
+    }
+    
+    @IBAction func clearButton(_ sender: Any) {
+        performSegue(withIdentifier: "eightth", sender: nil)
+    }
+    
+    
 }
 
